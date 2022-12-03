cbb664e20eff8152dd06c39941ae2b208dcea91d
Ozsel75 <ozsel75@hotmail.com>
2022-12-03 10:50:43 +0300

    redux store

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# React-Redux Store App
+## Build with Redux and fake store api 
+
+## :)
+
+<p>Simple store app</p>
+
+## How it looks
+
+![Screen](./screen.gif)
---
+++ src/containers/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="kelle ui fixed">
+      <div className="ui container center">
+        <h2 className="shop">SHOP -- SHOP </h2>
+      </div>
+      <div className="social">
+        <a href="https://github.com/Ozsel75" target="_blank">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/oz-sel-328909255/" target="_blank">
+          <i className="fa-brands fa-linkedin-in"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
